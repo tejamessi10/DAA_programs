495ccfcc86ca517dac7e2707ae046c83ffb33642
tejamessi10 <saitejaandapally@gmail.com>
2023-08-17 14:23:21 +0530

    shortest path problems

---
+++ floyd_warshall.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+void floydWarshall(int graph[20][20], int n)
+{
+    int i, j, k;
+    for (k = 0; k < n; k++)
+    {
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if (graph[i][j] > graph[i][k] + graph[k][j])
+                    graph[i][j] = graph[i][k] + graph[k][j];
+            }
+        }
+    }
+}
+ 
+int main(void)
+{
+    int n, i, j,graph[20][20];
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+    
+    printf("Enter the edges: \n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("[%d][%d]: ", i, j);
+            scanf("%d", &graph[i][j]);
+        }
+    }
+    printf("The original graph is:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d ", graph[i][j]);
+        }
+        printf("\n");
+    }
+    floydWarshall(graph, n);
+    printf("The shortest path matrix is:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d ", graph[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
